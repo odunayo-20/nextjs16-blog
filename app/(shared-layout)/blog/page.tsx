ddb0edf6e9df275d8a3883de79a2c5bc65588b90
mecbilltech <mecbilltech@gmail.com>
2026-04-04 11:28:04 +0100

    small changes

--- app/(shared-layout)/blog/page.tsx
+++ app/(shared-layout)/blog/page.tsx
@@ -6,15 +6,11 @@ import { Skeleton } from '@/components/ui/skeleton';
 import { api } from '@/convex/_generated/api'
 import { fetchQuery } from 'convex/nextjs';
 import { Metadata } from 'next';
-import { cacheLife, cacheTag } from 'next/cache';
 import Image from 'next/image';
 import Link from 'next/link';
 import { connection } from 'next/server';
-import { Suspense } from 'react';
 
 
-// export const dynamic = 'force-dynamic';
-// export const revalidate = 30; // Revalidate every 30 seconds
 
 export const metadata: Metadata = {
     title: 'Blog | Next.js 16 Tutorial',
@@ -51,9 +47,11 @@ export default async function BlogPage() {
 
 async function LoadBlogList(){
 
-     "use cache";
-    cacheLife("hours");
-    cacheTag('blog');
+    //  "use cache";
+    // cacheLife("hours");
+    // cacheTag('blog');
+
+    await connection();
 
     
     await new Promise((resolve) => setTimeout(resolve, 5000));
